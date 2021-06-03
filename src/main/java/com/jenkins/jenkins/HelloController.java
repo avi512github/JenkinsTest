98d23c1fecd35971dfbee439059b1aa4f685ab2c
avi512github <pappukumarpk512@gmail.com>
2021-06-03 13:15:43 +0530

    change pom commit

--- src/main/java/com/jenkins/jenkins/HelloController.java
+++ src/main/java/com/jenkins/jenkins/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 public class HelloController {
 
-	@GetMapping("/world")
+	@GetMapping
 	public String getHello() {
 		return "Hello World";
 	}
